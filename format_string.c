507a2904c5a22e18f6808a8a7e90f18b1fdfdc15
IceWizard4902 <77266084+IceWizard4902@users.noreply.github.com>
2022-07-19 23:17:23 +0800

    Third test

--- format_string.c
+++ format_string.c
@@ -1,103 +1,47 @@
-#include <unistd.h>
-#include <string.h>
+// gcc -Wno-stack-protector -no-pie -z execstack loophole.c -o loophole
+
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-#include <signal.h>
+#include <string.h>
 
-#define MAX_TITLE_LENGTH 10
-#define IS_DEBUG 0
+#define BUFSIZE 64
 
-char *filename = "fst_secrets.txt";
-int permission = IS_DEBUG;
+char buf1[BUFSIZE], buf2[BUFSIZE];
 
-void setup();
-void filegone();
+void bof()
+{
+	int i1, i2, i;
+	int byte1, byte2;
+	char buf[BUFSIZE];
+	printf("Buffer starts at: %p\n", buf);
+  fflush(stdout);
 
-void print_whole_file(FILE *fptr) {
-    char c;
-    while ((c = fgetc(fptr)) != EOF) {
-        printf("%c", c);
-    }
+	memset(buf, 0, BUFSIZE);
+	byte1 = fread(buf1, 1, BUFSIZE, stdin);
+	byte2 = fread(buf2, 1, BUFSIZE, stdin);
+
+	buf1[BUFSIZE-1] = 0;
+	buf2[BUFSIZE-1] = 0;
+
+	if(byte1 != byte2)
+	{
+		printf("Reading different number of bytes from both inputs!\n");
+		return;
+	}
+
+	for(i = 0; i < byte1 + byte2; i++)
+	{
+		i1 = (i % 2) ? BUFSIZE - 1 : i / 2;
+		i2 = (i % 2) ? i / 2 : BUFSIZE - 1;
+		buf[i] = buf1[i1] + buf2[i2];
+	}
+
 }
 
-// Check all titles collected
-void check_log() {
-    FILE *fp;
-    fp = fopen(filename, "r");
+int main(void)
+{
 
-    if (fp == NULL)
-        filegone();
+  bof();
 
-    print_whole_file(fp);
-
-    fclose(fp);
+	return 0;
 }
-
-// SUPER SAFE method to read string from user
-char *get_user_input() {
-    char *buf = malloc(MAX_TITLE_LENGTH + 1);
-    fgets(buf, MAX_TITLE_LENGTH, stdin);
-    fflush(stdin);
-    buf[MAX_TITLE_LENGTH] = 0;    // Null terminate
-    return buf;
-}
-
-void get_title() {
-    int *perm = &permission;
-    FILE *fp;
-    fp = fopen(filename, "a");    // Open my file of secrets
-
-    if (fp == NULL)
-        filegone();
-
-    char *input;
-    input = get_user_input();     // Get user input safely
-
-    char result[64] = "";
-    char prefix_format[8] = "%s";
-    char prefix[8] = "--> ";
-
-    strcat(result, prefix_format);
-    strcat(result, input);
-
-    fprintf(fp, result, prefix);
-    fclose(fp);
-
-    if (*perm)
-        check_log();
-}
-
-int main() {
-    setup();
-
-    printf("Format String Theory is going to be the next big hit!\n");
-    printf("Can you help me with the title of my papers? I need quite a few...\n\n");
-
-    int permission = IS_DEBUG;
-
-    while(1)
-        get_title();
-
-    return 0;
-}
-
-void filegone() {
-    printf("File is gone?!?!?!\n");
-    exit(1);
-}
-
-void timeout() {
-    printf("Time's up :(\n");
-    exit(0);
-}
-
-void setup() {
-    setvbuf(stdin,  NULL, _IONBF, 0);   // Switch off I/O buffering
-    setvbuf(stdout, NULL, _IONBF, 0);   // Switch off I/O buffering
-    setvbuf(stderr, NULL, _IONBF, 0);   // Switch off I/O buffering
-
-    signal(SIGALRM, timeout);           // Set program to exit on timeout
-    alarm(30);                          // Exit program after 30s
-}
-
