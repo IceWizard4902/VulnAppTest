ae2f5ef2abcf0b024a1413d7bf5d7d6633c93a87
IceWizard4902 <77266084+IceWizard4902@users.noreply.github.com>
2022-07-19 23:43:11 +0800

    Finalize BOF simple test

--- buffer_overflow2.c
+++ buffer_overflow2.c
@@ -4,15 +4,7 @@
 
 int main(void) {
     int a = 64;
-    
-    int b = a + 1;
-    int c = a + 2;
-    int d = a + 3;
-    int e = a + 4;
-
     int f = 128;
-
 	char buf[a];
-	memset(buf, 0, a);
-    buf[f] = 'l'
+    buf[f] = 90;
 }
